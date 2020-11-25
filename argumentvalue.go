9252d5846f0204a9d9b3a7ec374be09c9f9ae95c
Elizalde Baguinon <mizmerize@yahoo.com>
2020-11-25 11:14:26 +0800

    Update argument value

--- argumentvalue.go
+++ argumentvalue.go
@@ -7,13 +7,13 @@ import (
 
 // ArgumentValue - argument value
 type ArgumentValue struct {
-	value string
+	Value string
 }
 
 // Bool - Return boolean from value
 func (av *ArgumentValue) Bool() bool {
 
-	s := strings.ToLower(av.value)
+	s := strings.ToLower(av.Value)
 
 	if s == "true" || s == "on" || s == "yes" || s == "1" || s == "-1" {
 		return true
@@ -24,17 +24,17 @@ func (av *ArgumentValue) Bool() bool {
 
 // Int64 - Return int64 from value
 func (av *ArgumentValue) Int64() int64 {
-	v, _ := strconv.ParseInt(av.value, 10, 64)
+	v, _ := strconv.ParseInt(av.Value, 10, 64)
 	return v
 }
 
 // Float64 - Return float64 from value
 func (av *ArgumentValue) Float64() float64 {
-	v, _ := strconv.ParseFloat(av.value, 64)
+	v, _ := strconv.ParseFloat(av.Value, 64)
 	return v
 }
 
 // String - return string from value
 func (av *ArgumentValue) String() string {
-	return av.value
+	return av.Value
 }
